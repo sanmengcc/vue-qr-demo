8ccce42609163541e593aeda86a8249c64e724a6
wangzainan <wangzainan@outlook.com>
2022-05-11 17:37:00 +0800

    初始化登录

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,12 +1,10 @@
 <template>
-  //导航标签
-  <el-tabs v-model="activeName" @tab-click="handleClick">
-    <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
-    <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
-    <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
-    <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
-  </el-tabs>
-  //内容
+<!--  <el-tabs v-model="activeName" @tab-click="handleClick">-->
+<!--    <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>-->
+<!--    <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>-->
+<!--    <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>-->
+<!--    <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>-->
+<!--  </el-tabs>-->
   <el-table
       :data="tableData"
       border
